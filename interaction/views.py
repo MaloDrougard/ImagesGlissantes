d7436ec4abc3f82249d8fac11193058aab8d74a2
Malo <malcommalo.drougard@epfl.ch>
2017-10-28 12:40:03 +0200

    first circle interaction and django settings

---
+++ interaction/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+import datetime
+
+from django.http import HttpResponse
+from django.template import loader
+
+
+def interact(request):
+    now = datetime.datetime.now()
+    template = loader.get_template('interact.html')
+    context = {
+        'time' : now,
+    }
+    return HttpResponse(template.render(context, request))
+
+
